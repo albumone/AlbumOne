0139bff06b33c32e9176a65ca8b0769bdea1a992
Martins Grunskis <martins@grunskis.com>
2018-04-12 20:12:27 +0200

    Remove Stetho from Release builds

---
+++ app/src/release/java/com/grunskis/albumone/AlbumOneAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.grunskis.albumone;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class AlbumOneAppGlideModule extends AppGlideModule {
+}
